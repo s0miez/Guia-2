13529debb53a106d82e178dd06bc03fd3ec7ca54
Felipe <71887490+s0miez@users.noreply.github.com>
2023-09-07 00:01:36 -0300

    Create ejercicio3.rs

---
+++ ejercicio3.rs
@@ -0,0 +1,22 @@
+fn cuenta_vocales(texto: &str, letra: char) -> i32{
+    let mut contador = 0;
+
+    for caracter in texto.chars(){
+        if caracter == letra{
+            contador = contador + 1
+        }
+    }
+
+    return contador
+}
+
+fn main(){
+
+    let cadena_texto = String::from("Hola Mundo cruel");
+    let letra = 'a' && 'e' && 'i' && 'o' && 'u';
+    let numero = cuenta_vocales(&cadena_texto, letra);
+    println!("{} tiene {} vocales", cadena_texto, numero);
+
+}
+
+// ta malo arreglar saludos,,,
